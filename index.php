75ecd2d8876bb15a3c444fc7300a14d8c7381655
kernel64 <aymen.kernel@gmail.com>
2015-07-15 03:07:25 +0100

    backup

--- index.php
+++ index.php
@@ -4,4 +4,12 @@
  * User: Mohamed Aymen Ben Slimane <aymen.kernel@gmail.com>
  * Date: 11/07/15
  * Time: 12:27 م
- */ 
+ */
+
+use Moon\Application;
+
+require_once __DIR__.'/vendor/autoload.php';
+
+$app = new Application(true);
+
+$app->run();
